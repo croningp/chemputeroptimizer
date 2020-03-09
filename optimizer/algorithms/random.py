a4532988b1d6ecffda519df9a7aff28642b07fe9
ArtemLeonov <artem.leonov@glasgow.ac.uk>
2020-03-09 12:21:11 +0000

    Updated Random search algorithm to match new Algorithm API

--- optimizer/algorithms/random.py
+++ optimizer/algorithms/random.py
@@ -2,15 +2,26 @@
 
 import random
 
+import numpy as np
+
 from ..algorithms import AbstractAlgorithm
 
 class Random_(AbstractAlgorithm):
     def __init__(self, max_iterations):
         super().__init__(max_iterations)
 
-    def optimize(self):
-        print("Current setup", self.current_setup)
-        print("Constraints", self.setup_constraints)
+    def optimize(self, parameters, results, constraints=None):
+
+        print(
+            'random optimizer for the following parameters: \n',
+            f'parameters: {parameters} \n',
+            f'results: {results} \n',
+            f'contstrains: {constraints} \n'
+        )
+
+        return np.array(
+            [random.uniform(a, b) for a, b in constraints]
+        )
 
     def initialise(self):
         pass
