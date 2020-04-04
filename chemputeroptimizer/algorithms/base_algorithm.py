ea67a5f2ff3fa1112ee3dfe1371d8c9173f2f399
ArtemLeonov <artem.leonov@glasgow.ac.uk>
2020-04-04 17:09:00 +0100

    Included default config in AbstractAlgorithm

--- chemputeroptimizer/algorithms/base_algorithm.py
+++ chemputeroptimizer/algorithms/base_algorithm.py
@@ -13,30 +13,39 @@ class AbstractAlgorithm(ABC):
     the experimental parameters, save the iteration for further access.
 
     Attributes:
-        dimensions (List[Tuple[int, int]], optinal): Search space dimensions,
-            list of (min, max) tuples for each input data point.
+        dimensions (Iterable[Iterable[float, float]], optinal): Search space
+            dimensions, e.g. list of (min, max) tuples for each input data point.
+        config (Dict, optional): All neccessary attributes for specific
+            algorithm. If not supplied, default class attribute is
+            used instead.
     """
-    def __init__(self, dimensions=None):
+    # all config attributes neccessary for specific algorithm
+    DEFAULT_CONFIG = {}
+
+    def __init__(self, dimensions=None, config=None):
         if not hasattr(self, 'name'): self.name = self.__class__.__name__
         self.logger = logging.getLogger(f'optimizer.algorithm.{self.name}')
         self.dimensions = dimensions
+        self.config = self.DEFAULT_CONFIG.copy()
+        if config:
+            self.config.update(config)
 
     @abstractmethod
     def optimize(self, parameters, results, constraints=None):
         """Find the parameters for the next iteration.
 
         Uses the experimental matrixes to find new parameter set through
-        given optimisation algorithm. Replaces the worst parameter with new points
-        setting the target value to -1.
+        given optimisation algorithm.
+
         This method has to be redefined in ancestor classes.
 
         Args:
-            parameters (:obj: np.array): (n x i) size matrix where n is number of experiments and i
-                is number of experimental parameters.
-            results (:obj: np.array): (n x j) size matrix where j is number of the target parameters.
+            parameters (:obj: np.array): (n x i) size matrix where n is number
+                of experiments and i is number of experimental parameters.
+            results (:obj: np.array): (n x j) size matrix where j is number
+                of the target parameters.
             constraints (Any): tuple with min/max values for the parameters
 
         Returns:
             (np.array): An array with new set of experimental input parameters.
         """
-
