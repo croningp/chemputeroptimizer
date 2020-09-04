515796a4fd67298937c46ebc476055c040c03672
Alex Hammer <2423935H@student.gla.ac.uk>
2020-09-04 13:47:43 +0100

    WIP add RunHPLC step

--- chemputeroptimizer/platform/steps/steps_analysis/run_hplc.py
+++ chemputeroptimizer/platform/steps/steps_analysis/run_hplc.py
@@ -1,5 +1,5 @@
 from typing import Any, Tuple, Dict
-
+from time import sleep
 from xdl.steps.base_steps import AbstractBaseStep
 
 
@@ -7,6 +7,7 @@ class RunHPLC(AbstractBaseStep):
 
     PROP_TYPES = {
         'hplc': str,
+        'valve': str,
         'on_finish': Any,
         'protocol': Tuple[str, Dict],
     }
@@ -14,6 +15,7 @@ class RunHPLC(AbstractBaseStep):
     def __init__(
             self,
             hplc: str,
+            valve: str,
             on_finish: Any,
             protocol: Tuple[str, Dict] = None,
             **kwargs
@@ -25,7 +27,18 @@ class RunHPLC(AbstractBaseStep):
 
     def execute(self, chempiler: 'Chempiler', logger=None, level=0):
         hplc = chempiler[self.hplc]
-        hplc.get_spectrum(self.protocol)
+        idex = chempiler[self.valve]
+        hplc.switch_method(self.protocol)
+        hplc.preprun()
+        while not hplc.status()[0] == 'PRERUN':
+            sleep(1)
+        idex.sample(5)
+        if self.protocol == 'AH_cleaning':
+            sleep(5*60)
+        else:
+            sleep(30*60)
+        hplc.standby()
+        hplc.get_spectrum()
         spec = hplc.spectrum.default_processing()
         self.on_finish(spec)
         return True
