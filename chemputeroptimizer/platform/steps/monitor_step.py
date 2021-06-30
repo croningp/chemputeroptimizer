067ae5fbb247bcb0b0cf1f97d7c55d8ed57c7c6d
Emma Clarke <2167935c@student.gla.ac.uk>
2021-06-30 15:48:54 +0100

    WIP updated monitor step to run with raman

--- chemputeroptimizer/platform/steps/monitor_step.py
+++ chemputeroptimizer/platform/steps/monitor_step.py
@@ -1,10 +1,14 @@
 import time
 from typing import List, Callable, Optional, Dict, Any
 
+from networkx import MultiDiGraph
+
 from xdl.errors import XDLError
 from xdl.steps.base_steps import AbstractStep, AbstractDynamicStep, Step
+from xdl.steps.special_steps import Async, Await
 from chemputerxdl.steps import HeatChill, HeatChillToTemp, Wait, StopHeatChill, Transfer, StartStir, Stir
-#from xdl.steps.steps_analysis import RunNMR
+from chemputeroptimizer.platform.steps.utils import find_instrument
+from chemputeroptimizer.platform.steps import RunRaman
 
 # from .utils import SpectraAnalyzer
 
@@ -29,7 +33,8 @@ class Monitor(AbstractDynamicStep):
     def __init__(
         self,
         children: List[Step],
-        methods: List,
+        method: str = "Raman",
+        sampling_delay = 2.0,
         time: bool = False,
         sample_volume: float = None,
         threshold: float = 0.05,
@@ -50,75 +55,62 @@ class Monitor(AbstractDynamicStep):
             'done': False,
         }
 
-    def _get_instrument(self, method):
-        """Obtain the instrument for a provided analytical method."""
+        self.method = method
+        self.sampling_delay = sampling_delay
 
-    def _get_sample_volume(self):
-        """Get the sample volume based on the analytical instrument used."""
+    def on_prepare_for_execution(self, graph: MultiDiGraph) -> None:
+
+        self.instrument = find_instrument(graph, self.method)
+
 
     def _get_analysis_steps(self, method):
         """Get all steps required to obtained analytical data for a given method."""
+
+        # Raman
+        # no special prepartion needed, just measure the spectrum
+        if self.method == 'Raman':
+            return [
+                RunRaman(
+                    raman=self.instrument,
+                    on_finish=self._on_analysis_finish,
+                )
+            ]
+        else:
+            raise NotImplementedError(f"{method} is not a supported method.")
 
     def _on_analysis_finish(self, result: List[float]):
         """Check if reaction is done according to the difference in the current
         and last analysis spectrum. Set state['done'] to True if rxn is complete.
         Set state['current_state'] to the new spectrum.
         """
-        if (self.state['current_state'] - result) < self.threshold:
-            self.state['done'] = True
-        self.state['current_state'] = result
+        pass
+
+    def _on_child_finish(self):
+        """Check if reaction is done according to the difference in the current
+        and last analysis spectrum. Set state['done'] to True if rxn is complete.
+        Set state['current_state'] to the new spectrum.
+        """
+        self.state['done'] = True
 
     def on_start(self):
         # Not optimising time, just execute steps.
-        if not self.time:
-            return self.children
 
-        # HeatChill step, start heating/chilling and move onto on_continue loop.
-        if type(self.step) == HeatChill:
-            return [
-                HeatChillToTemp(self.step.vessel,
-                                self.step.temp,
-                                stir=self.step.stir)
-            ]
-
-        # Wait step, move straight onto on_continue loop.
-        elif type(self.step) == Wait:
-            return []
-
-        elif type(self.step) == Stir:
-            return [
-                StartStir(self.step.vessel, self.step.stir_speed)
-            ]
+        return [Async(
+            pid="monitoring", 
+            children=[self.step], 
+            on_finish=self._on_child_finish)]
 
     def on_continue(self):
         # Rxn is complete or not recording time, finish.
-        if self.state['done'] or not self.time:
+        if self.state['done']:
             return []
 
         # Get start_t and continuously run NMR until rxn is complete.
         self.start_t = time.time()
 
-        analysis_steps = [
-            self._get_analysis_steps(method) for method in self.methods
-        ]
+        time.sleep(self.sampling_delay)
 
-        return analysis_steps
+        return self._get_analysis_steps(self.method)
 
     def on_finish(self):
-        # Not recording time, finish.
-        if not self.time:
-            return []
-
-        # Send elapsed time to on_time_obtained callback and finish.
-        self.end_t = time.time()
-        self.on_time_obtained(self.end_t - self.start_t)
-
-        # Stop heating/chilling and finish.
-        if type(self.step) == HeatChill:
-            return [
-                StopHeatChill(vessel=self.step.vessel)
-            ]
-
-        # No heating/chilling, just finish.
-        else:
-            return []
+        return []
