8fa469a163476edf91aed14609dad9f7effea277
ArtemLeonov <artem.leonov@glasgow.ac.uk>
2021-07-06 11:48:22 +0100

    Update RunRaman with comments, type annotations, minor cleanup

--- chemputeroptimizer/platform/steps/steps_analysis/run_raman.py
+++ chemputeroptimizer/platform/steps/steps_analysis/run_raman.py
@@ -5,6 +5,13 @@ implementation from the AnalyticalLabware.
 
 from typing import Optional, Callable
 
+from AnalyticalLabware.devices.OceanOptics.Raman.raman_spectrum import (
+    RamanSpectrum,
+)
+from AnalyticalLabware.devices.OceanOptics.Raman.raman_control import (
+    OceanOpticsRaman,
+)
+
 from xdl.steps.base_steps import AbstractBaseStep
 
 
@@ -12,37 +19,39 @@ class RunRaman(AbstractBaseStep):
 
     PROP_TYPES = {
         'raman': str,
-        'on_finish': Callable,
+        'on_finish': Callable[[RamanSpectrum], None],
         'blank': bool,
+        'background_path': str,
     }
 
     def __init__(
             self,
             raman: str,
-            on_finish: Optional[Callable] = None,
+            on_finish: Optional[Callable[[RamanSpectrum], None]] = None,
             blank: bool = False,
+            background_path: Optional[str] = None,
             **kwargs
     ):
         super().__init__(locals())
-
-        self.background_path = None
-
-        for key, value in kwargs.items():
-            setattr(self, key, value)
 
     def locks(self, chempiler):
         return [], [], []
 
     def execute(self, chempiler: 'Chempiler', logger=None, level=0):
-        raman = chempiler[self.raman]
+        """Measure raman spectrum and pass the result to on_finish."""
 
+        raman: OceanOpticsRaman = chempiler[self.raman]
+
+        # If the background path is given, load the data and store it as
+        # A new reference
         if self.background_path:
             raman.spectrum.load_data(self.background_path)
-            if raman.spectrum.reference is None:
-                raman.spectrum.reference = raman.spectrum.y
+            raman.spectrum.reference = raman.spectrum.y
 
+        # If blank - just update the reference
         if self.blank:
             raman.obtain_reference_spectrum()
+        # Else just upload the spectrum
         else:
             raman.get_spectrum()
 
@@ -52,7 +61,7 @@ class RunRaman(AbstractBaseStep):
 
             raman.spectrum.default_processing()
 
-        if self.on_finish:
+        if self.on_finish is not None:
             self.on_finish(raman.spectrum.copy())
 
         return True
