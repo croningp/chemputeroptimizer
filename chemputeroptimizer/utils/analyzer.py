Fixed novelty calculation if known regions are absent

chemputeroptimizer/utils/analyzer.py
@@ -502,7 +502,7 @@ target peak, resolving')
             # Rounding to neglect small differences in ppm scales
             # Across several spectra
             regions_expanded = expand_peak_regions(regions)
-            regions_expanded_xs = np.around(spectrum.x[regions_expanded], 4)
+            regions_expanded_xs = np.around(spectrum.x[regions_expanded], 3)
 
             information_score = calculate_information_score(regions, areas)
 

chemputeroptimizer/utils/novelty_search.py
@@ -94,7 +94,12 @@ def calculate_novelty_coefficient(
     novelty_diff = np.setdiff1d(spectrum_peaks_region, spectra_regions)
 
     # Calculating the coefficient
-    novelty_coef = len(novelty_diff)/len(spectrum_peaks_region) + \
-        1/len(spectra_regions)
+    try:
+        novelty_coef = len(novelty_diff)/len(spectrum_peaks_region) + \
+            1/len(spectra_regions)
+    # In case no reference regions were given
+    # E.g. first spectrum is evaluated
+    except ZeroDivisionError:
+        novelty_coef = len(novelty_diff)/len(spectrum_peaks_region)
 
     return novelty_coef