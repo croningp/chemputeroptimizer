38012742f8b69486715bdf47d78e2511d6207d43
ArtemLeonov <artem.leonov@glasgow.ac.uk>
2022-02-03 09:52:42 +0000

    Quickfix: added missing pydoe2 requirement

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
         "numpy",
         "scikit-learn==0.22", # TODO remove when incompatibility problems are resolved between sklearn and skopt
         "scikit-optimize>=0.8",
+        "pyDOE2",
         "AnalyticalLabware",
     ]
 )
